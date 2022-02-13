2a0942bd0bdecdb698dae240fdd88c5361cbd3b9
Adam Koewler <xadamxk@gmail.com>
2022-02-12 22:44:34 -0600

    Add remaining time

--- MMM-CHAMPIONSQUEUE-LEADERBOARD.js
+++ MMM-CHAMPIONSQUEUE-LEADERBOARD.js
@@ -1,108 +1,127 @@
 Module.register("MMM-CHAMPIONSQUEUE-LEADERBOARD", {
-	// Default module config
-	defaults: {
-		updateInterval: 60, // minutes
-		// lang: config.language,
-		resultCount: 10, // Number of players to list
-		showTitle: true, // Season - Split (ex. 2022 Spring Season - Split 1)
-		showCurrentSplit: true, // Limit leaderboard to current split (vs current season)
-		showRemainingTime: true, // Show remaining time for given split/season
-		showPlayerWinRates: true, // Default
-		showPlayerWinCount: false, // Off by default
-		showPlayerLP: true // Default
-	},
+  // Default module config
+  defaults: {
+    updateInterval: 60, // minutes
+    // lang: config.language,
+    resultCount: 10, // Number of players to list
+    showTitle: true, // Season - Split (ex. 2022 Spring Season - Split 1)
+    showCurrentSplit: true, // Limit leaderboard to current split (vs current season)
+    showRemainingTime: true, // Show remaining time for given split/season
+    showPlayerWinRates: true, // Default
+    showPlayerWinCount: false, // Off by default
+    showPlayerLP: true // Default
+  },
 
-	// TODO: Styling for title, remaining time, rows
-	// TODO: Add win rates to player rows
-	// TODO: Add win counts to player rows
-	// TODO: Add player lp
+  // TODO: Styling for title, remaining time, rows
+  // TODO: Add win rates to player rows
+  // TODO: Add win counts to player rows
+  // TODO: Add player lp
+  // TODO: Change title depending on showCurrentSplit
 
-	// Module properties
-	players: [],
-	year: new Date().getFullYear(),
-	seasonId: 0,
-	seasonTitle: "",
-	seasonShort: "",
-	seasonOpenDate: null,
-	seasonCloseDate: null,
-	splitId: 0,
-	splitTitle: "",
-	splitOpenDate: null,
-	splitCloseDate: null,
+  // Module properties
+  players: [],
+  year: new Date().getFullYear(),
+  seasonId: 0,
+  seasonTitle: "",
+  seasonShort: "",
+  seasonOpenDate: null,
+  seasonCloseDate: null,
+  seasonDaysRemaining: 0,
+  splitId: 0,
+  splitTitle: "",
+  splitOpenDate: null,
+  splitCloseDate: null,
+  splitDaysRemaining: 0,
 
-	// Start the module.
-	start: function () {
-		// Get initial API data
-		this.getData();
+  // Start the module.
+  start: function () {
+    // Get initial API data
+    this.getData();
 
-		// Schedule update poll
-		var self = this;
-		setInterval(function () {
-			self.getData();
-		}, self.config.updateInterval * 60 * 1000); //convert to milliseconds
-	},
+    // Schedule update poll
+    var self = this;
+    setInterval(function () {
+      self.getData();
+    }, self.config.updateInterval * 60 * 1000); //convert to milliseconds
+  },
 
-	getTranslations() {
-		return {
-			en: "translations/en.json"
-		};
-	},
-	getStyles: function () {
-		return ["MMM-CHAMPIONSQUEUE-LEADERBOARD.css"];
-	},
-	getTemplate() {
-		return `templates/standings.njk`;
-	},
-	getTemplateData() {
-		return {
-			players: this.players,
-			seasonId: this.seasonId,
-			seasonTitle: this.seasonTitle,
-			seasonShort: this.seasonShort,
-			seasonOpenDate: this.seasonOpenDate,
-			seasonCloseDate: this.seasonCloseDate,
-			splitId: this.splitId,
-			splitTitle: this.splitTitle,
-			splitOpenDate: this.splitOpenDate,
-			splitCloseDate: this.splitCloseDate,
-			config: this.config
-		};
-	},
+  getTranslations() {
+    return {
+      en: "translations/en.json"
+    };
+  },
+  getStyles: function () {
+    return ["MMM-CHAMPIONSQUEUE-LEADERBOARD.css"];
+  },
+  getTemplate() {
+    return `templates/standings.njk`;
+  },
+  getTemplateData() {
+    return {
+      players: this.players,
+      seasonId: this.seasonId,
+      seasonTitle: this.seasonTitle,
+      seasonShort: this.seasonShort,
+      seasonOpenDate: this.seasonOpenDate,
+      seasonCloseDate: this.seasonCloseDate,
+      seasonDaysRemaining: this.seasonDaysRemaining,
+      splitId: this.splitId,
+      splitTitle: this.splitTitle,
+      splitOpenDate: this.splitOpenDate,
+      splitCloseDate: this.splitCloseDate,
+      splitDaysRemaining: this.splitDaysRemaining,
+      config: this.config
+    };
+  },
 
-	// Fetch schedule for provided tournament ids
-	getData: function () {
-		this.sendSocketNotification("MMM-CHAMPIONSQUEUE-LEADERBOARD-GET-STANDINGS");
-	},
-	// Schedule data is coming back
-	socketNotificationReceived: function (notification, payload) {
-		if (notification === "MMM-CHAMPIONSQUEUE-LEADERBOARD-STANDINGS") {
-			this.getStandingData(payload);
-		}
-	},
-	// Condense standing data and render it
-	getStandingData: function (data) {
-		const resultCount = this.config.resultCount;
-		if (!data || !data.hasOwnProperty("leaderboards") || data.leaderboards.length < 1) {
-			return []; // Service is down or endpoint changed
-		}
-		const currentLeaderboard = data["leaderboards"][0];
-		// Season
-		this.seasonId = currentLeaderboard["seasonId"];
-		this.seasonTitle = currentLeaderboard["title"];
-		this.seasonShort = currentLeaderboard["shortTitle"];
-		this.year = currentLeaderboard["year"];
-		this.seasonOpenDate = currentLeaderboard["openDate"];
-		this.seasonCloseDate = currentLeaderboard["closeDate"];
-		// Split
-		const currentSplit = currentLeaderboard["split"];
-		this.splitId = currentSplit["splitId"];
-		this.splitTitle = currentSplit["title"];
-		this.splitOpenDate = currentSplit["openDate"];
-		this.splitCloseDate = currentSplit["closeDate"];
+  // Fetch schedule for provided tournament ids
+  getData: function () {
+    this.sendSocketNotification("MMM-CHAMPIONSQUEUE-LEADERBOARD-GET-STANDINGS");
+  },
+  // Schedule data is coming back
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "MMM-CHAMPIONSQUEUE-LEADERBOARD-STANDINGS") {
+      this.getStandingData(payload);
+    }
+  },
+  // Condense standing data and render it
+  getStandingData: function (data) {
+    const resultCount = this.config.resultCount;
+    if (
+      !data ||
+      !data.hasOwnProperty("leaderboards") ||
+      data.leaderboards.length < 1
+    ) {
+      return []; // Service is down or endpoint changed
+    }
+    const currentSeason = data["leaderboards"][0];
+    // Season
+    this.seasonId = currentSeason["seasonId"];
+    this.seasonTitle = currentSeason["title"];
+    this.seasonShort = currentSeason["shortTitle"];
+    this.year = currentSeason["year"];
+    this.seasonOpenDate = currentSeason["openDate"];
+    this.seasonCloseDate = currentSeason["closeDate"];
+    const seasonClose = new Date(currentSeason["closeDate"]);
+    const today = new Date();
+    this.seasonDaysRemaining = Math.round(
+      (seasonClose.getTime() - today.getTime()) / (1000 * 3600 * 24)
+    );
 
-		const results = currentLeaderboard.hasOwnProperty("lineup") && currentLeaderboard["lineup"];
-		this.players = results.slice(0, resultCount);
-		console.log(this.players);
-		this.updateDom(500);
-	}
+    // Split
+    const currentSplit = currentSeason["split"];
+    this.splitId = currentSplit["splitId"];
+    this.splitTitle = currentSplit["title"];
+    this.splitOpenDate = currentSplit["openDate"];
+    this.splitCloseDate = currentSplit["closeDate"];
+    const splitClose = new Date(currentSplit["closeDate"]);
+    this.splitDaysRemaining = Math.round(
+      (splitClose.getTime() - today.getTime()) / (1000 * 3600 * 24)
+    );
+
+    const results =
+      currentSeason.hasOwnProperty("lineup") && currentSeason["lineup"];
+    this.players = results.slice(0, resultCount);
+    this.updateDom(500);
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -9,16 +9,19 @@ var NodeHelper = require("node_helper");
 var fetch = require("node-fetch");
 
 module.exports = NodeHelper.create({
-	// Override socketNotificationReceived method.
-	socketNotificationReceived: function (notification, payload) {
-		let self = this;
-		const url = "https://d1fodqbtqsx6d3.cloudfront.net/leaderboards.json";
-		if (notification === "MMM-CHAMPIONSQUEUE-LEADERBOARD-GET-STANDINGS") {
-			fetch(url)
-				.then((res) => res.json())
-				.then((json) => {
-					self.sendSocketNotification("MMM-CHAMPIONSQUEUE-LEADERBOARD-STANDINGS", json);
-				});
-		}
-	}
+  // Override socketNotificationReceived method.
+  socketNotificationReceived: function (notification, payload) {
+    let self = this;
+    const url = "https://d1fodqbtqsx6d3.cloudfront.net/leaderboards.json";
+    if (notification === "MMM-CHAMPIONSQUEUE-LEADERBOARD-GET-STANDINGS") {
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => {
+          self.sendSocketNotification(
+            "MMM-CHAMPIONSQUEUE-LEADERBOARD-STANDINGS",
+            json
+          );
+        });
+    }
+  }
 });
